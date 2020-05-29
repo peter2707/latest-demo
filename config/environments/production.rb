895f7722b7e3bebe80311ad48a03b46f5edfc066
Peter2707 <s3848409@student.rmit.edu.au>
2020-05-29 17:45:25 +1000

    Changed redirect to method

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,8 +75,8 @@ Rails.application.configure do
     :address => 'smtp.sendgrid.net',
     :port => '587',
     :authentication => :plain,
-    :user_name => ENV['app172786799@heroku.com'],
-    :password => ENV['kqlphpnx3867'],
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
     :domain => 'heroku.com',
     :enable_starttls_auto => true
   }
